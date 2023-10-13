b91cd3e7d46cf1b41fe6ede9fbf801ef73f58adc
Uaplexer <rupleflex31@gmail.com>
2023-10-13 20:43:12 +0300

    models and views added

--- Backend/user_api/models.py
+++ Backend/user_api/models.py
@@ -1,3 +1,8 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    city = models.CharField(max_length=64, default='Київ')
+    phone_number = models.CharField(max_length=13, default='')
--- Backend/user_api/views.py
+++ Backend/user_api/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import CustomUser
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+@api_view(['POST'])
+def reg_view(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    first_name = request.data.get('first_name')
+    last_name = request.data.get('last_name')
+    if not any([email, password, last_name, first_name]):
+        return Response({'error': 'Please provide all required fields: email, password, first_name, last_name.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+    user, created = CustomUser.objects.get_or_create(email=email, username=email.split('@')[0], first_name=first_name,
+                                                     last_name=last_name, city=request.data.get('city', 'Київ'),
+                                                     phone_number=request.data.get('phone_number', ''))
+    if created:
+        user.set_password(password)
+        user.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            'access_token': str(refresh.access_token),
+            'refresh_token': str(refresh),
+        }, status=status.HTTP_201_CREATED)
+    return Response({'error': 'Email already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout_view(request):
+    try:
+        refresh_token = request.data['refresh_token']
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+        return Response({'message': 'Logged out successfully.'}, status=status.HTTP_200_OK)
+    except Exception:
+        return Response({'error': 'Invalid refresh token.'}, status=status.HTTP_400_BAD_REQUEST)
