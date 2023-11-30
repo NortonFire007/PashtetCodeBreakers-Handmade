6535fe2200976ce958e8ff28f29f262cdbb41d59
YuriyChestnyh <yurachestnyh@gmail.com>
2023-11-30 21:06:23 +0200

    new version with charity

--- add product page/JS/charitycalculator.js
+++ add product page/JS/charitycalculator.js
@@ -2,27 +2,32 @@ function updateCharity() {
     // Получаем значение введенной цены
     const price = document.getElementById("value").value;
 
+    // Получаем выбранный процент благотворительности
+    const selectedPercentage = document.getElementById("charityPercentage").value;
+
     // Проверяем, является ли введенное значение допустимым числом
-    if (!isNaN(price) && price !== "") {
-        // Вычисляем 15% от цены
-        const charityAmount = (parseFloat(price) * 0.15).toFixed(0);
+    if (!isNaN(price) && price !== "" && !isNaN(selectedPercentage) && selectedPercentage !== "") {
+      // Вычисляем сумму для благотворительности
+      const charityAmount = (parseFloat(price) * (parseFloat(selectedPercentage) / 100)).toFixed(0);
 
-        // Обновляем поле "charity" вычисленным значением
-        document.getElementById("charity").value = charityAmount;
+      // Обновляем поле "charity" вычисленным значением
+      document.getElementById("charity").value = charityAmount;
     } else {
-        // Если введенное значение не является допустимым числом, очищаем поле "charity"
-        document.getElementById("charity").value = "";
+      // Если введенные значения не являются допустимыми числами, очищаем поле "charity"
+      document.getElementById("charity").value = "";
     }
-}
-document.getElementById("value").addEventListener("input", updateCharity);
+  }
 
-document.getElementById("charity").addEventListener("input", function() {
+  document.getElementById("value").addEventListener("input", updateCharity);
+  document.getElementById("charityPercentage").addEventListener("change", updateCharity);
+
+  document.getElementById("charity").addEventListener("input", function() {
     const enteredCharity = parseFloat(this.value);
-    const minCharity = parseFloat(document.getElementById("value").value) * 0.15;
+    const minCharity = parseFloat(document.getElementById("value").value) * (parseFloat(document.getElementById("charityPercentage").value) / 100);
 
-    // Проверяем, является ли введенное значение числом и не меньше 15% от цены
+    // Проверяем, является ли введенное значение числом и не меньше указанного процента от цены
     if (isNaN(enteredCharity) || enteredCharity < minCharity) {
-        // Устанавливаем минимальное значение в 15% от цены
-        this.value = minCharity.toFixed(0);
+      // Устанавливаем минимальное значение в указанном проценте от цены
+      this.value = minCharity.toFixed(0);
     }
-});
+  });
